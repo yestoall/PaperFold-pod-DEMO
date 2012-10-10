a61b00ea3f629282aa9ec5586a3015768bac5dd6
nacho <nacho@yestoall.com>
2012-10-10 12:23:18 +0200

    icons

--- app/controllers/wish_view_controller.rb
+++ app/controllers/wish_view_controller.rb
@@ -1,7 +1,10 @@
 class WishViewController < UIViewController
   def initWithTabBar
     me = init
-    me.tabBarItem = UITabBarItem.alloc.initWithTitle("wish", image:nil, tag:2)
+    img_on  = "tabbar_icon_wish".uiimage
+    img_off = "tabbar_icon_wish_off".uiimage
+    me.tabBarItem = UITabBarItem.alloc.initWithTitle("wish", image:nil, tag:3)
+    me.tabBarItem.setFinishedSelectedImage(img_on, withFinishedUnselectedImage:img_off)
     me
   end
 
