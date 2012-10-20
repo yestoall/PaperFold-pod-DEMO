029bd3f891c31d90053264aa818a52d4bccc4a04
yestoall <nacho@yestoall.com>
2012-10-20 15:23:21 +0200

    demo2 working

--- demo2/app/controllers/app_controller.rb
+++ demo2/app/controllers/app_controller.rb
@@ -1,34 +1,51 @@
 
 class AppController < UIViewController
 
+  MENU_TABLE    = 1
+  CONTENT_TABLE = 2
+
   def init
 
+    @menu = [
+      { "title" => "menu 1" },
+      { "title" => "menu 2" },
+      { "title" => "menu 3" },
+      { "title" => "menu 4" },
+      { "title" => "menu 5" },
+      { "title" => "menu 6" }
+    ]
+
     @data = [
-      { "id" => 1,  "name" => "one" },
-      { "id" => 2,  "name" => "two" },
-      { "id" => 3,  "name" => "three" }
+      { "id" => 1,  "name" => "option one" },
+      { "id" => 2,  "name" => "option two" },
+      { "id" => 3,  "name" => "option three" }
     ]
 
     bounds = self.view.bounds.size
 
-    paper                 = PaperFoldView.alloc.initWithFrame([[0,0],[bounds.width,bounds.height]])
-    paper.backgroundColor = 0x000000.uicolor
-    paper.setAutoresizingMask(UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth)
-    self.view.addSubview(paper)
+    menu           = UITableView.alloc.initWithFrame([[0,0],[220,bounds.height]])
+    menu.tag       = MENU_TABLE
+    menu.rowHeight = 40
+    menu.setDelegate(self)
+    menu.setDataSource(self)
 
-    menu = Menu.alloc.init
-    paper.setLeftFoldContentView(menu.view)
-
-    center = UITableView.alloc.initWithFrame([[0,0],[bounds.width,bounds.height]])
+    center           = UITableView.alloc.initWithFrame([[0,0],[bounds.width,bounds.height]])
+    center.tag       = CONTENT_TABLE
     center.rowHeight = 80
-    paper.setCenterContentView(center)
     center.setDelegate(self)
     center.setDataSource(self)
 
+    paper                 = PaperFoldView.alloc.initWithFrame([[0,0],[bounds.width,bounds.height]])
+    paper.backgroundColor = 0x000000.uicolor
+    paper.setAutoresizingMask(UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth)
+
+    paper.setLeftFoldContentView(menu)
+    paper.setCenterContentView(center)
     paper.setDelegate(self)
     paper.setEnableRightFoldDragging(false)
-
     paper.setPaperFoldState(PaperFoldStateLeftUnfolded)
+
+    view << paper
 
     self
   end
@@ -40,11 +57,24 @@ class AppController < UIViewController
   end
 
   def tableView(tableView, numberOfRowsInSection:section)
-    @data ? @data.length : 0
+    case tableView.tag
+    when MENU_TABLE
+      @menu ? @menu.length : 0
+    when CONTENT_TABLE
+      @data ? @data.length : 0
+    end
   end
 
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    cellID = "cell"
+    case tableView.tag
+    when MENU_TABLE
+      cellID = "cell_menu"
+      data = @menu[indexPath.row]["title"]
+    when CONTENT_TABLE
+      cellID = "cell_content"
+      data = @data[indexPath.row]["name"]
+    end
+
     cell = tableView.dequeueReusableCellWithIdentifier(cellID) || begin
       cell                = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:cellID)
       cell.accessoryType  = UITableViewCellAccessoryDisclosureIndicator
@@ -52,8 +82,7 @@ class AppController < UIViewController
       cell
     end
 
-    item = @data[indexPath.row]
-    cell.textLabel.text = item["name"]
+    cell.textLabel.text = data
     cell
   end
 
